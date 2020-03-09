2c5ea43d069ea18c4c504d1b40bc34b650c4d3ba
Thomas Wu <wu0512@yahoo.com>
2020-03-09 00:12:53 -0400

    Use scss as css comilpler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,9 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
-
+  config.assets.css_compressor = :sass
+  
+  config.assets.version = '1.1' # was '1.0'
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 

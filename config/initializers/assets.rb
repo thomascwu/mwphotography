8afcf552c028bd58bb8cc72f00bedbd6c95fb856
Thomas Wu <wu0512@yahoo.com>
2020-03-08 22:57:34 -0400

    Revert "Creative css"

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,6 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('css')
-
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
@@ -14,4 +12,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( creative.css )
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :gives
   resources :abouts
   resources :voices
-  get 'voices/index'
+  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'voices#index'
+  root 'welcome#index'
 end
